0ebf24931eb45b96da53f611bbb03fd2a4c769f5
Daniel Barbosa Maranhão <dbm2@cin.ufpe.br>
2018-10-08 14:15:41 -0300

    Implementing Euler methods

--- models/methods/euler.py
+++ models/methods/euler.py
@@ -1,6 +1,46 @@
 import sys
+from sympy import sympify, solveset, symbols
 from models.enums.methodstype import MethodsType
 from models.methods.method import Method
 
 class Euler(Method):
-	debug = 1
+
+	def __init__(self, type, y, t, stepsSize, steps, function):
+		self.type = type
+		self.y = y
+		self.t = t
+		self.stepsSize = stepsSize
+		self.steps = steps
+		self.function = sympify(function)
+
+	def calculate(self):
+		for i in range(1, self.steps + 1):
+			v = (0, 0)
+			if self.type == MethodsType.EULER:
+				v = self.calculateByEuler(self.y[i-1], self.t[i-1])
+			elif self.type == MethodsType.EULER_INVERSE:
+				v = self.calculateByInverseEuler(self.y[i-1], self.t[i-1])
+			elif self.type == MethodsType.EULER_IMPROVED:
+				v = self.calculateByImprovedEuler(self.y[i-1], self.t[i-1])
+
+			print(v)
+			self.t.append(v[0])
+			self.y.append(v[1])
+	
+	def calculateByEuler(self, previousY, previousT):
+		nextT = previousT + self.stepsSize
+		nextY = previousY + self.stepsSize * float(self.function.subs([("y", previousY), ("t", previousT)]))
+		return (nextT, nextY)
+
+	def calculateByInverseEuler(self, previousY, previousT):
+		nextT = previousT + self.stepsSize
+		predictionNextY = self.calculateByEuler(previousY, previousT)[1]
+		nextY = previousY + self.stepsSize * float(self.function.subs([("y", predictionNextY), ("t", nextT)]))
+		return (nextT, nextY)
+		
+	def calculateByImprovedEuler(self, previousY, previousT):
+		nextT = previousT + self.stepsSize
+		predictionNextY = self.calculateByEuler(previousY, previousT)[1]
+		nextY = previousY + self.stepsSize * (float(self.function.subs([("y", previousY), ("t", previousT)])) + float(self.function.subs([("y", predictionNextY), ("t", nextT)])))/2
+		return (nextT, nextY)
+		
--- project.py
+++ project.py
@@ -4,3 +4,5 @@ from models.input import Input
 entry = Input("resources/entrada.txt")
 entry.parseAndAssembleMethods()
 
+entry.methods[2].calculate()
+
