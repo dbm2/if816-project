22dbeaa5c7f2594fb8de17e20ae332262bafb1a6
Daniel Barbosa Maranhão <dbm2@cin.ufpe.br>
2018-10-17 20:00:17 -0300

    Improving chart draw

--- project.py
+++ project.py
@@ -6,20 +6,22 @@ from view.chart import Chart
 print("- Projeto de Metodos Numericos.")
 print("- Autor: Daniel Barbosa Maranhao | dbm2 | 107.698.344-85\n")
 
-print("Iniciando leitura do arquivo resources/entrada.txt...\n")
+print("Iniciando leitura do arquivo resources/entrada.txt...")
 entry = Input("resources/entrada.txt")
 entry.parseAndAssembleMethods()
 
-print("Calculando estimativas dos metodos numericos...\n")
+print("Calculando estimativas dos metodos numericos...")
 for method in entry.methods:
 	method.calculate()
 
-print("Escrevendo resultados dos metodos no arquivo resources/saida.txt...\n")
+print("Escrevendo resultados dos metodos no arquivo resources/saida.txt...")
 exit = Output("resources/saida.txt", entry.methods)
 exit.transcriptMethods()
 
-print("Gerando grafico comparativo dos metodos no arquivo resources/chart.png...\n")
+print("Gerando graficos comparativso dos metodos na pasta resources/charts...\n")
 view = Chart(entry.methods)
+view.drawForEachMethod()
+view.drawForAllMethods()
 
 print("Pronto!")
 
--- view/chart.py
+++ view/chart.py
@@ -4,19 +4,40 @@ import matplotlib.pyplot as plt
 class Chart:
 
 	def __init__(self, methods):
+		self.methods = methods
 
+	def drawForAllMethods(self):
 		fig, ax = plt.subplots()
 		fig.set_size_inches(10.0, 6.0)
 
-		for method in methods:
+		for method in self.methods:
 			name = method.type.getName()
 			color = method.type.getColor()
 			ax.plot(method.y, method.t, lw = 2, color = color, label = name)
 
-		ax.set_title('Estimativas para os Métodos')
+		ax.set_title('Estimativas para os Metodos')
 		ax.set_xlabel('valores de y')
 		ax.set_ylabel('valores de t')
 
 		ax.legend()
 		
 		fig.savefig('resources/chart.png', dpi=100)
+
+
+	def drawForEachMethod(self):
+		for method in self.methods:
+			fig, ax = plt.subplots()
+			fig.set_size_inches(5.0, 5.0)
+
+
+			name = method.type.getName()
+			color = method.type.getColor()
+			ax.plot(method.y, method.t, lw = 2, color = color, label = name)
+
+			ax.set_title('Estimativas para o Metodo')
+			ax.set_xlabel('valores de y')
+			ax.set_ylabel('valores de t')
+
+			ax.legend()
+		
+			fig.savefig('resources/charts/chart_' + method.type.value + '.png' , dpi=100)
